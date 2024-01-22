b3e7db96c03e50f4a24eb23338aa156659360512
NaserMJ <nassermemarjavid@gmail.com>
2024-01-22 18:26:21 +0100

    2.push gyro acc gui

--- src/main/java/com/lern1/mysensors/MainActivity.kt
+++ src/main/java/com/lern1/mysensors/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.lern1.mysensors
 
+import android.content.Context
 import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.location.Location
 import android.location.LocationRequest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -32,9 +38,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
+import androidx.core.content.getSystemService
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationServices
@@ -46,15 +54,21 @@ class MainActivity : ComponentActivity() {
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private lateinit var locationCallback : LocationCallback
 
+    private lateinit var sensorManager: PackageManager
+
     private var lat  by mutableStateOf("")
     private var lon by  mutableStateOf("")
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
         getLocation()
+
+        val sensorEventListener = object : SensorEventListener {
+            override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+            override fun onSensorChanged(event: SensorEvent) {}
+        }
 
         setContent {
             MySensorsTheme {
@@ -63,8 +77,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    Column {
+                        Gps(lat, lon)
+                        AccelerometerAndGyroscope()
+                    }
 
-                    Gps(lat, lon)
                 }
             }
         }
@@ -103,7 +120,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Gps(lat:String, lon:String){
+fun Gps(lat:String, lon:String) {
 
     //var loc by remember { mutableStateOf<Location?>(null) }
 
@@ -124,12 +141,7 @@ fun Gps(lat:String, lon:String){
                 horizontalArrangement =  Arrangement.SpaceBetween,
                 verticalAlignment     = Alignment.CenterVertically
             ){
-                Icon(imageVector = Icons.Default.LocationOn,
-                    contentDescription = "GPS Icon",
-                )
-                Text(text = "Sensorname", color = Color.Black )
-                Switch(checked = false, onCheckedChange = {
-                })
+                Text(text = "GPS", color = Color.Black )
             }
             Row(
                 modifier = Modifier.fillMaxWidth(),
@@ -149,7 +161,7 @@ fun Gps(lat:String, lon:String){
                             modifier = Modifier.width(100.dp))
                     }
                 }
-                Row {
+                Column {
                     Button(onClick = { }, content = { Text(text = "Save")})
                     Button(onClick = { }, content = { Text(text = "show")})
                 }
@@ -163,13 +175,107 @@ fun Gps(lat:String, lon:String){
             }
         }
     }
-}
 
+} // End of GPS Composable
+
+@Composable
+fun AccelerometerAndGyroscope() {
+
+    val ctx = LocalContext.current
+    val sensorManager: SensorManager = ctx.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+    var accX = remember { mutableStateOf(0F) }
+    var accY = remember { mutableStateOf(0F) }
+    var accZ = remember { mutableStateOf(0F) }
+    /*
+        var gyrX = remember { mutableStateOf(0F) }
+        var gyrY = remember { mutableStateOf(0F) }
+        var gyrZ = remember { mutableStateOf(0F) }*/
+
+    val sensorEventListener = object : SensorEventListener {
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+        }
+
+        override fun onSensorChanged(event: SensorEvent) {
+            if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+                accX.value = event.values[0]
+                accY.value = event.values[1]
+                accZ.value = event.values[2]
+            } else if (event.sensor.type == Sensor.TYPE_GYROSCOPE) {
+                // gyrX.value = event.values[0]
+                // gyrY.value = event.values[1]
+                // gyrZ.value = event.values[2]
+            }
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+            .height(150.dp)
+        //.background(color = Color.Gray)
+    ){
+        Column() {
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement =  Arrangement.SpaceBetween,
+                verticalAlignment     = Alignment.CenterVertically
+            ){
+                Text(text = "GPS", color = Color.Black )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement =  Arrangement.SpaceBetween,
+            ) {
+                Column() {
+                    Row {
+                        Text(text = "accX:   ",
+                            modifier = Modifier.width(40.dp))
+                        Text(text = "lat",
+                            modifier = Modifier.width(100.dp))
+                    }
+                    Row {
+                        Text(text = "accY:   ",
+                            modifier = Modifier.width(40.dp))
+                        Text(text = "lon",
+                            modifier = Modifier.width(100.dp))
+                    }
+                    Row {
+                        Text(text = "accZ:   ",
+                            modifier = Modifier.width(40.dp))
+                        Text(text = "lon",
+                            modifier = Modifier.width(100.dp))
+                    }
+                }
+                Column {
+                    Button(onClick = { }, content = { Text(text = "Save")})
+                    Button(onClick = { }, content = { Text(text = "show")})
+                }
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = "Phase:")
+                Slider(value =20f, onValueChange = {} )
+                Text(text = "0")
+            }
+        }
+    }
+
+
+
+
+
+
+}// End of ACC&GYR Composable
 
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MySensorsTheme {
+        Gps(lat = "55", lon = "66")
+        AccelerometerAndGyroscope()
     }
 }
