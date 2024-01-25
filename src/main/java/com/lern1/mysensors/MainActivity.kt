c0b8d03f42f900fc2c7ed2cb1fd84232f1db1968
NaserMJ <nassermemarjavid@gmail.com>
2024-01-25 12:10:22 +0100

    6.puash daten gespeichert und angezeigt

--- src/main/java/com/lern1/mysensors/MainActivity.kt
+++ src/main/java/com/lern1/mysensors/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.lern1.mysensors
 
+import android.app.AlertDialog
 import android.content.Context
 import android.content.pm.PackageManager
 import android.hardware.Sensor
@@ -9,9 +10,9 @@ import android.hardware.SensorManager
 import android.location.Location
 import android.location.LocationRequest
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -21,16 +22,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -42,21 +40,26 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
-import androidx.core.content.getSystemService
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationServices
 import com.lern1.mysensors.ui.theme.MySensorsTheme
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
 
 class MainActivity : ComponentActivity() {
 
-    private lateinit var locationRequest: LocationRequest
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private lateinit var locationCallback : LocationCallback
+    private lateinit var locationRequest: LocationRequest
 
 
     private var lat  by mutableStateOf("")
     private var lon by  mutableStateOf("")
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -64,17 +67,19 @@ class MainActivity : ComponentActivity() {
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
         getLocation()
 
+
+
         setContent {
             MySensorsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
                     Column {
                         Gps(lat, lon)
                         Accelerometer()
                         Gyroscope()
+                       Magnetometer()
                     }
 
                 }
@@ -114,10 +119,39 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+private fun saveDataToFile(context: Context, fileName: String, dataList: List<String>) {
+    try {
+        val file = File(context.filesDir, fileName)
+        val fileWriter = FileWriter(file)
+
+        for (data in dataList) {
+            fileWriter.append(data)
+        }
+
+        fileWriter.flush()
+        fileWriter.close()
+
+        // Optionally, display a toast or log that data has been saved.
+        Toast.makeText(context, "Data saved in file", Toast.LENGTH_SHORT).show()
+    } catch (e: IOException) {
+        e.printStackTrace()
+        // Handle the exception
+        Toast.makeText(context, "Error saving data", Toast.LENGTH_SHORT).show()
+    }
+}
+
+private fun showDataDialog(context: Context, dataList: List<String>) {
+    AlertDialog.Builder(context)
+        .setTitle("Accelerometer Data")
+        .setMessage(dataList.joinToString(separator = "\n"))
+        .setPositiveButton("OK") { _, _ -> }
+        .show()
+}
+
+
+
 @Composable
 fun Gps(lat:String, lon:String) {
-
-    //var loc by remember { mutableStateOf<Location?>(null) }
 
     println("posX$lat")
     println("posY$lon")
@@ -179,27 +213,53 @@ fun Accelerometer() {
     val ctx = LocalContext.current
     val sensorManager: SensorManager = ctx.getSystemService(Context.SENSOR_SERVICE) as SensorManager
 
-    var checkAcc = false
+    var acx = remember { mutableStateOf(-1f) }
+    var acy = remember { mutableStateOf(-1f) }
+    var acz = remember { mutableStateOf(-1f) }
 
-    var accX = remember { mutableStateOf(-1f) }
-    var accY = remember { mutableStateOf(-1f) }
-    var accZ = remember { mutableStateOf(-1f) }
+    val accelerationDataList = remember { mutableListOf<String>() }
 
 
     val sensorEventListener = object : SensorEventListener {
         override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+
         override fun onSensorChanged(event: SensorEvent) {
             if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
-                accX.value = event.values[0]
-                accY.value = event.values[1]
-                accZ.value = event.values[2]
+                acx.value = event.values[0]; acy.value = event.values[1]; acz.value = event.values[2]
+
+                val timestamp = System.currentTimeMillis()
+                val accX = event.values[0]
+                val accY = event.values[1]
+                val accZ = event.values[2]
+
+                // Format the data as a string
+                val data = "Timestamp: $timestamp\n" +
+                        "accX: $accX\n" +
+                        "accY: $accY\n" +
+                        "accZ: $accZ\n\n"
+
+                // Add the data to the list
+                accelerationDataList.add(data)
             }
         }
     }
-    sensorManager.registerListener(sensorEventListener,sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),10000000)
+    DisposableEffect(sensorManager) {
+        val accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        val sensorDelay = 10000000
+
+        sensorManager.registerListener(sensorEventListener, accelerometerSensor, sensorDelay)
+
+        onDispose {
+            sensorManager.unregisterListener(sensorEventListener)
+        }
+    }
+
 
     Box(
-        modifier = Modifier.fillMaxWidth().padding(15.dp).height(150.dp)
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+            .height(150.dp)
         //.background(color = Color.Gray)
     ){
         Column() {
@@ -212,26 +272,22 @@ fun Accelerometer() {
                 modifier = Modifier.fillMaxWidth(), horizontalArrangement =  Arrangement.SpaceBetween,
             ) {
                 Column() {
-                    Text("accX: ${accX.value}\naccY: ${accY.value}\naccZ: ${accZ.value}")
+                    Text("accX: ${acx.value}\naccY: ${acy.value}\naccZ: ${acz.value}")
                 }
                 Column {
-                    Button(onClick = { }, content = { Text(text = "Save")})
-                    Button(onClick = { }, content = { Text(text = "show")})
+                    Button(onClick = { saveDataToFile(ctx, "acceleration_data.txt", accelerationDataList) }, content = { Text(text = "Save")})
+                    Button(onClick = { showDataDialog(ctx, accelerationDataList) }, content = { Text(text = "show")})
                 }
-            }
-            Row(
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(text = "Phase:")
-                Slider(value =20f, onValueChange = {} )
-                Text(text = "0")
             }
         }
     }
 
 
 
-}// End of ACC&GYR Composable
+}// End of ACC Composable
+
+
+
 
 @Composable
 fun Gyroscope(){
@@ -239,52 +295,147 @@ fun Gyroscope(){
     val ctx = LocalContext.current
     val sensorManager: SensorManager = ctx.getSystemService(Context.SENSOR_SERVICE) as SensorManager
 
-    var checkGyr = false
+    var gX = remember { mutableStateOf(-1f) }
+    var gY = remember { mutableStateOf(-1f) }
+    var gZ = remember { mutableStateOf(-1f) }
 
-    var gyrX = remember { mutableStateOf(0f) }
-    var gyrY = remember { mutableStateOf(0f) }
-    var gyrZ = remember { mutableStateOf(0f) }
+    var gyroscopeDataList = remember { mutableListOf<String>() }
 
     val sensorEventListener = object : SensorEventListener {
         override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+
         override fun onSensorChanged(event: SensorEvent) {
             if (event.sensor.type == Sensor.TYPE_GYROSCOPE) {
-                gyrX.value = event.values[0]
-                gyrY.value = event.values[1]
-                gyrZ.value = event.values[2]
+
+                gX.value = event.values[0]; gY.value = event.values[1]; gZ.value = event.values[2]
+
+                val timestamp = System.currentTimeMillis()
+                val gyrX = event.values[0]
+                val gyrY = event.values[1]
+                val gyrZ = event.values[2]
+
+                // Format the data as a string
+                val data = "Timestamp: $timestamp\n" +
+                        "gyrX: $gyrX\n" +
+                        "gyrY: $gyrY\n" +
+                        "gyrZ: $gyrZ\n\n"
+
+                // Add the data to the list
+                gyroscopeDataList.add(data)
             }
         }
     }
-    sensorManager.registerListener(sensorEventListener,sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),10000000)
-    sensorManager.registerListener(sensorEventListener,sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE),10000000)
+    DisposableEffect(sensorManager) {
+        val accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+        val sensorDelay = 10000000
 
-    Box(
-        modifier = Modifier.fillMaxWidth().padding(15.dp).height(150.dp)
+        sensorManager.registerListener(sensorEventListener, accelerometerSensor, sensorDelay)
+
+        onDispose {
+            sensorManager.unregisterListener(sensorEventListener)
+        }
+    }
+
+
+        Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+            .height(150.dp)
         //.background(color = Color.Gray)
     ){
         Column() {
             Row (
-                modifier = Modifier.fillMaxWidth(), horizontalArrangement =  Arrangement.SpaceBetween, verticalAlignment     = Alignment.CenterVertically
+                modifier = Modifier.fillMaxWidth(), horizontalArrangement =  Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically
             ){
-                Text(text = "GPS", color = Color.Black )
+                Text(text = "Gyroscope", color = Color.Black )
             }
             Row(
                 modifier = Modifier.fillMaxWidth(), horizontalArrangement =  Arrangement.SpaceBetween,
             ) {
                 Column() {
-                    Text("X: ${gyrX.value}\nY: ${gyrY.value}\nZ: ${gyrZ.value}")
+                    Text("X: ${gX.value}\nY: ${gY.value}\nZ: ${gZ.value}")
                 }
                 Column {
-                    Button(onClick = { }, content = { Text(text = "Save")})
-                    Button(onClick = { }, content = { Text(text = "show")})
+                    Button(onClick = { saveDataToFile(ctx, "gyroscope_data.txt", gyroscopeDataList) }, content = { Text(text = "Save")})
+                    Button(onClick = { showDataDialog(ctx, gyroscopeDataList) }, content = { Text(text = "show")})
                 }
             }
+
+        }
+    }
+
+}// End of GYR Composable
+
+@Composable
+fun Magnetometer(){
+
+
+    val ctx = LocalContext.current
+    val sensorManager: SensorManager = ctx.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+    var mX = remember { mutableStateOf(-1f) }
+    var mY = remember { mutableStateOf(-1f) }
+    var mZ = remember { mutableStateOf(-1f) }
+
+    var magnetometerDataList = remember { mutableListOf<String>() }
+
+    val sensorEventListener = object : SensorEventListener {
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+        override fun onSensorChanged(event: SensorEvent) {
+            if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
+                mX.value = event.values[0]; mY.value = event.values[1]; mZ.value = event.values[2]
+
+                val timestamp = System.currentTimeMillis()
+                val magX = event.values[0]
+                val magY = event.values[1]
+                val magZ = event.values[2]
+
+                // Format the data as a string
+                val data = "Timestamp: $timestamp\n" +
+                        "accX: $magX\n" +
+                        "accY: $magY\n" +
+                        "accZ: $magZ\n\n"
+
+                // Add the data to the list
+                magnetometerDataList.add(data)
+            }
+        }
+    }
+    DisposableEffect(sensorManager) {
+        val accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
+        val sensorDelay = 10000000
+
+        sensorManager.registerListener(sensorEventListener, accelerometerSensor, sensorDelay)
+
+        onDispose {
+            sensorManager.unregisterListener(sensorEventListener)
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+            .height(150.dp)
+        //.background(color = Color.Gray)
+    ){
+        Column() {
+            Row (
+                modifier = Modifier.fillMaxWidth(), horizontalArrangement =  Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically
+            ){
+                Text(text = "Magnetometer", color = Color.Black )
+            }
             Row(
-                verticalAlignment = Alignment.CenterVertically
+                modifier = Modifier.fillMaxWidth(), horizontalArrangement =  Arrangement.SpaceBetween,
             ) {
-                Text(text = "Phase:")
-                Slider(value =20f, onValueChange = {} )
-                Text(text = "0")
+                Column() {
+                    Text("X: ${mX.value}\nY: ${mY.value}\nZ: ${mZ.value}")
+                }
+                Column {
+                    Button(onClick = { saveDataToFile(ctx, "magnetometer_data.txt", magnetometerDataList) }, content = { Text(text = "Save")})
+                    Button(onClick = { showDataDialog(ctx, magnetometerDataList) }, content = { Text(text = "show")})
+                }
             }
         }
     }
